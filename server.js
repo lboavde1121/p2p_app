7efcfdd8f556f3e7235d58b5c418b9637ea52f77
Ryo Tsukahara <lboavde1121@gmail.com>
2018-06-28 02:17:03 +0900

    [add] initial commit. TODO move to docker

---
+++ server.js
@@ -0,0 +1,41 @@
+// Dependencies
+const express = require('express');
+const createHttpServer = require('http').createServer;
+const createIo = require('socket.io');
+const createPeerServer = require('peer').ExpressPeerServer;
+
+// Environment
+const port = process.env.PORT || 59798;
+
+// Routes
+const app = express();
+const httpServer = createHttpServer(app);
+const io = createIo(httpServer);
+const peerServer = createPeerServer(httpServer);
+
+app.use('/api', peerServer);
+app.use(express.static(__dirname));
+
+// Boot
+httpServer.listen(port, () => {
+  console.log(`Boot on http://localhost:${port}`);
+});
+
+// Manage p2p keys
+const keys = [];
+peerServer.on('connection', (key) => {
+  keys.push(key);
+
+  console.log('connected', keys);
+
+  io.emit('keys', keys);// 接続中の全てのクライアントへ`keys`イベントを発行
+});
+peerServer.on('disconnect', (key) => {
+  const index = keys.indexOf(key);
+  if (index > -1) {
+    keys.splice(index, 1);
+  }
+  console.log('disconnect', keys);
+
+  // io.emit('keys', keys);
+});
